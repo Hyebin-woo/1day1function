47965c22f2c68077fe8e00a8af19b468a4b1c1f9
Hyebin-Woo <dev.hyebin@gmail.com>
2022-05-27 23:53:33 +0900

    add12

---
+++ 12_배열특정값.js
@@ -0,0 +1,20 @@
+// 100개 중에 8이 총 몇개일까?
+function numCount(num) {
+  num = num.toString();
+
+  let count = 0;
+
+  for (let i = 1; i <= 100; i++) {
+    const arr = i.toString().split("");
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] === num) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+}
+
+//numCount(8);
+
+//   (Array(100).fill(1).map((value, index) => value + index) + '').split('8').length-1;
