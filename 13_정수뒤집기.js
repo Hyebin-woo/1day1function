c47d0e26528e3fc6dba231f0d96ef6ee916aca7c
Hyebin-Woo <dev.hyebin@gmail.com>
2022-05-28 12:08:34 +0900

    add13

---
+++ 13_정수뒤집기.js
@@ -0,0 +1,12 @@
+// 자연수 뒤집기
+function reverseArr(num) {
+  const arr = num
+    .toString()
+    .split("")
+    .reverse()
+    .map((x) => parseInt(x));
+
+  console.log(arr);
+  // return arr;
+}
+reverseArr(54321);
